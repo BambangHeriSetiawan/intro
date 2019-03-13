bbb73547f82371f99a47a9f2c6a3a58215e5b4fc
Bambang Heri Setiawan <setiawan.heri.bambang@gmail.com>
2019-03-13 14:01:31 +0700

    add sample, fix layout

--- app/src/main/java/com/simxid/into/MainActivity.kt
+++ app/src/main/java/com/simxid/into/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.simxid.into
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import android.widget.Toast
+import com.simxid.intro.app.IntroActivity
+import com.simxid.intro.slide.FragmentSlide
+
+class MainActivity : IntroActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        isFullscreen = true
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        addSlide(
+            FragmentSlide.Builder()
+            .background(R.color.colorPrimary).fragment(R.layout.intro_one)
+            .canGoBackward(false)
+            .canGoForward(true)
+            .build())
+        addSlide(FragmentSlide.Builder()
+            .background(R.color.colorPrimary).fragment(R.layout.intro_two)
+            .canGoBackward(true)
+            .canGoForward(true)
+            .build())
+        addSlide(FragmentSlide.Builder()
+            .background(R.color.colorPrimary).fragment(R.layout.intro_three)
+            .canGoBackward(true)
+            .canGoForward(true)
+            .build())
+        addSlide(FragmentSlide.Builder()
+            .background(R.color.colorPrimary).fragment(R.layout.intro_four)
+            .canGoBackward(true)
+            .canGoForward(true)
+            .build())
+        isButtonBackVisible = false
     }
+
+    fun toas(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
 }
